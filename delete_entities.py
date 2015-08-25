0b37c53818ea98842f9cfa42c046e4cef430af54
Eleonore9 <eleonore.mayola@ymail.com>
2015-08-25 12:17:13 +0100

    Comments to delete_entities.py

--- delete_entities.py
+++ delete_entities.py
@@ -5,9 +5,7 @@ from requests.auth import HTTPBasicAuth
 
 URL = "https://www.getembed.com/4/"
 
-test_properties = ["25a1cd92-e9b2-429e-8f0b-d5b0fec0a70c", "0f8ac79a-ad97-40c3-b0b4-bb59b5e0027d",
-                   "542576ff-4b81-4517-8dbd-8f0c67cfe91c", "7f6c4159-fe5b-43d8-9040-70b66d4f58a1",
-                   "df0f772c-e591-4294-962e-7d22411c380a", "cf5efbc0-034e-44a3-9563-5a45dea33328"]
+ENTITIES = [u'xxxxxxxx', u'zzzzzz', u'yyyyyyy', u'vvvvvvvv'] ## Replace w/ entity ids
 
 def list_properties(filename):
     "List ids of properties to be deleted."
@@ -32,5 +30,5 @@ if __name__ == "__main__":
     l = list_properties("to_delete.csv")
     delete_properties(l, sys.argv[1], sys.argv[2])
 
-    #delete_properties(test_properties, sys.argv[1], sys.argv[2])
+    #delete_properties(ENTITIES, sys.argv[1], sys.argv[2])
 
