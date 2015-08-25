876b0e5ae2a162c81e63a92d00b70819f167a650
Eleonore9 <eleonore.mayola@ymail.com>
2015-08-25 12:29:06 +0100

    Comments to upload_measurments.py

--- upload_measurements.py
+++ upload_measurements.py
@@ -11,10 +11,11 @@ URL = "https://www.getembed.com/4/"
 ## Example:
 ## [{"entity1": ["device1.1", "device1.2"]}, {"entity2": ["device2.1", "device2.2"]}]
 entities = [
-    {"ded6b5f1-ce79-4912-b6be-f8f9af7ce93b": ["ded6b5f1-ce79-4912-b6be-f8f9af7ce93b"]},
-    {"e5ffee9f-8ab9-4346-b012-78aea2ed9a73": ["1473d94f-bf97-4321-80b9-be84dedca6de"]}
+    {"xxxxxxxxxxxxxx-xx-xxx-xxxxxx": ["aaaaabbbbbbbbaabbbbbbbbaa"]},
+    {"yyyyyyyyyy-yyyy-yyyyyy-yyyyy": ["ccccccccccdddcddcdccdddcc"]}
 ]
 
+# Update measurements and make sure "type" matches current sensor type
 measurements = {"measurements": [
     {
         "value": "0.87",
@@ -31,6 +32,7 @@ def check_device(entity_id, device_id, user, pwd):
     r = requests.get(url=url, auth=auth)
     print r.status_code
     print r.json()
+
 
 ## To run to upload measurements device per device
 # NOTE: Update measurements content and make sure "type" matches current sensor type
@@ -59,7 +61,7 @@ def upload_all_measurements(user, pwd):
 
 
 if __name__ == "__main__":
-    post_measurements("45f93880-df89-4565-acd1-6a1d6c22792c", "d434ec0e-210f-4937-9873-6c42eddd3936", sys.argv[1], sys.argv[2])
+    post_measurements("yyyyyyyyyyyyyy", "gggggggggggggggg", sys.argv[1], sys.argv[2])
 
-    # check_device("45f93880-df89-4565-acd1-6a1d6c22792c", "d434ec0e-210f-4937-9873-6c42eddd3936", sys.argv[1], sys.argv[2])
+    # check_device("xxxxxxxxxxxxxxxx", "dddddddddddddd", sys.argv[1], sys.argv[2])
                 
